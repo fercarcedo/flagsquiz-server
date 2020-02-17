240dc7eb06a5da614427d48db3907d246e1f346e
Fernando García Álvarez <fercarcedo@gmail.com>
2020-02-17 14:12:36 +0100

    change request to req

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const db = pgp(process.env.DATABASE_URL);
 
 app.post('/api/logs', function(req, res) {
-    const body = request.body;
+    const body = req.body;
     db.one('INSERT INTO logs(correct, incorrect, time, score, time_until_first_correct, time_until_first, incorrect, max_consecutive_correct, max_consecutive_incorrect) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [body.correct, body.incorrect, body.time, body.score, body.time_until_first_correct, body.time_until_first_incorrect, body.max_consecutive_correct, body.max_consecutive_incorrect])
         .then(() => {
              res.send(200);
