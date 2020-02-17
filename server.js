5e01f29f71d080288545876ae1035718d338c4d6
Fernando García Álvarez <fercarcedo@gmail.com>
2020-02-17 14:33:20 +0100

    create table if not exists

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 const db = pgp(process.env.DATABASE_URL);
 
+db.none('CREATE TABLE IF NOT EXISTS logs(correct INTEGER NOT NULL, incorrect INTEGER NOT NULL, time INTEGER NOT NULL, score INTEGER NOT NULL, time_until_first_correct INTEGER NOT NULL, time_until_first_incorrect INTEGER NOT NULL, max_consecutive_correct INTEGER NOT NULL, max_consecutive_incorrect INTEGER NOT NULL)')
+    .then(() => console.log('Created table logs'));
+
 app.post('/api/logs', function(req, res) {
     const body = req.body;
-    db.one('INSERT INTO logs(correct, incorrect, time, score, time_until_first_correct, time_until_first, incorrect, max_consecutive_correct, max_consecutive_incorrect) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [body.correct, body.incorrect, body.time, body.score, body.time_until_first_correct, body.time_until_first_incorrect, body.max_consecutive_correct, body.max_consecutive_incorrect])
+    db.none('INSERT INTO logs(correct, incorrect, time, score, time_until_first_correct, time_until_first_incorrect, max_consecutive_correct, max_consecutive_incorrect) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [body.correct, body.incorrect, body.time, body.score, body.time_until_first_correct, body.time_until_first_incorrect, body.max_consecutive_correct, body.max_consecutive_incorrect])
         .then(() => {
              res.send(200);
         })
